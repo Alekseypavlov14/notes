8e1028f56731b4c88a99b01326ad6f0136eaa68a
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-19 18:27:55 +0300

    implemented delete-account-and-user-data feature

--- src/features/accounts/delete-account-and-user-data.ts
+++ src/features/accounts/delete-account-and-user-data.ts
@@ -1,3 +1,14 @@
+import { directoriesManipulator, filesManipulator } from '../file-system'
+import { directoriesRepository } from '@/entities/directories'
+import { filesRepository } from '@/entities/files'
+import { deleteAccount } from '@/app/auth'
+
 export async function deleteAccountAndUserData() {
-  // TODO
+  const files = await filesManipulator.getUserItems()
+  const directories = await directoriesManipulator.getUserItems()
+
+  await deleteAccount()
+
+  await Promise.all(files.map((file) => filesRepository.deleteById(file.id)))
+  await Promise.all(directories.map((directory) => directoriesRepository.deleteById(directory.id)))
 }
